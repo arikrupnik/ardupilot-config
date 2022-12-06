f1f30024c1f0f530c7ce0e7638bce19325d0e142
Ari Krupnik <ari@30pins.com>
2022-12-05 21:25:14 -0600

    better variable names

--- scripting/loose-end/disable_throttle_input.lua
+++ scripting/loose-end/disable_throttle_input.lua
@@ -7,33 +7,37 @@
 
 local MAV_SEVERITY = {EMERGENCY=0, ALERT=1, CRITICAL=2, ERROR=3, WARNING=4, NOTICE=5, INFO=6, DEBUG=7}
 
-local THROTTLE_CHANNEL = param:get("RCMAP_THROTTLE")
-if not THROTTLE_CHANNEL then
+local THROTTLE_CHANNEL_N = param:get("RCMAP_THROTTLE")
+if not THROTTLE_CHANNEL_N then
    gcs:send_text(MAV_SEVERITY.ERROR,
                  "RCMAP_THROTTLE parameter missing; throttle manipulation unavailable")
    return
 end
 
 -- if overrides are enabled, I want to set override value faster than
--- it times out; if there is no timeout, I set override once and go home
-local RC_OVERRIDE_TIME = param:get("RC_OVERRIDE_TIME")
-if RC_OVERRIDE_TIME == 0 then
+-- it times out; special values: 0 means overrides disabled; -1 means
+-- "will never time out," in this case I set override once and go home
+local RC_OVERRIDE_TIME_S = param:get("RC_OVERRIDE_TIME")
+if RC_OVERRIDE_TIME_S == 0 then
    gcs:send_text(MAV_SEVERITY.ERROR, string.format(
                     "RC_OVERRIDE_TIME=%f disables throttle override",
-                    RC_OVERRIDE_TIME))
+                    RC_OVERRIDE_TIME_S))
    return
 end
 
-local THROTTLE_MIN = param:get(string.format("RC%d_MIN", THROTTLE_CHANNEL))
+local THROTTLE_MIN = param:get(string.format("RC%d_MIN", THROTTLE_CHANNEL_N)) or 1000
+
+gcs:send_text(MAV_SEVERITY.NOTICE,
+              string.format("Throttle input override: %g", THROTTLE_MIN))
+
 
 function update(run_once)
-   rc:get_channel(THROTTLE_CHANNEL):set_override(THROTTLE_MIN)
+   rc:get_channel(THROTTLE_CHANNEL_N):set_override(THROTTLE_MIN)
    if not run_once then
-      return update, RC_OVERRIDE_TIME * 1000 / 2
+      local rc_override_time_ms = RC_OVERRIDE_TIME_S * 1000
+      return update, rc_override_time_ms / 2
    end
 end
 
-gcs:send_text(MAV_SEVERITY.NOTICE, string.format("Activating throttle input override: %g", THROTTLE_MIN))
-
 -- "-1 will never timeout"; run once without rescheduling
-return update(RC_OVERRIDE_TIME == -1)
+return update(RC_OVERRIDE_TIME_S == -1)
